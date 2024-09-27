7683819bf0b39efd216b5bf58cab5405516dbd10
elmer-riva <elmeraugustoriva@gmail.com>
2024-09-27 18:32:03 -0500

    feat(interventions): add interventions component with basic interface including table and buttons

--- src/app/service/pages/interventions/interventions.component.ts
+++ src/app/service/pages/interventions/interventions.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
 
 @Component({
   selector: 'app-interventions',
-  standalone: true,
-  imports: [],
   templateUrl: './interventions.component.html',
-  styleUrl: './interventions.component.css'
+  styleUrls: ['./interventions.component.css'],
 })
-export class InterventionsComponent {
+export class InterventionsComponent implements OnInit, AfterViewInit {
+  displayedColumns: string[] = ['clientFirstName', 'clientLastName', 'licensePlate', 'vehicleModel', 'registrationDate', 'completionDate', 'state'];
+  dataSource = new MatTableDataSource<any>();
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    // Placeholder for future data fetch
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
 }
