c3d40d8ba82f1f528eea041c1bbdcd64c9ed2b5b
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-09-27 11:49:31 -0500

    feat(tasks-page): add filtering, search, pagination, and data integration from TaskService
    - Implemented search and filtering functionality for tasks table.
    - Integrated pagination with `MatPaginator` for managing table data display.
    - Added sorting using `MatSort` for task columns.
    - Connected TaskService to retrieve and display data dynamically in the table.

--- src/app/service/services/task.service.ts
+++ src/app/service/services/task.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {BaseService} from "../../shared/services/base.service";
 import {Task} from "../model/task.entity";
+import {catchError, Observable, retry} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +11,10 @@ export class TaskService extends BaseService<Task> {
   constructor() {
     super();
     this.resourceEndpoint = '/tasks';
+  }
+
+  public getByMechanicId(mechanicId: number): Observable<Task> {
+    return this.http.get<Task>(`${this.resourcePath()}?assistant.id=${mechanicId}`, this.httpOptions)
+      .pipe(retry(2),catchError(this.handleError));
   }
 }
