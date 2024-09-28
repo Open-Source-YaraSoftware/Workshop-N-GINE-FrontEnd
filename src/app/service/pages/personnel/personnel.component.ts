c2647d07b13ca4bc6c469e756db456c29304e42f
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-09-28 14:23:46 -0500

    fix: implement reactivity data

--- src/app/service/pages/personnel/personnel.component.ts
+++ src/app/service/pages/personnel/personnel.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, computed, OnInit, signal} from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PersonnelService } from '../../services/personnel.service';
 import { Mechanic } from '../../model/mechanic.entity';
 import { NewMechanicDialogComponent } from '../../components/new-mechanic-dialog/new-mechanic-dialog.component';
 import { NgIf } from '@angular/common';
-import { MatFormField } from '@angular/material/form-field';
+import {MatFormField, MatLabel} from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatButton, MatIconButton } from '@angular/material/button';
@@ -23,21 +23,30 @@ import { PersonnelListComponent } from '../../components/personnel-list/personne
     MatIconButton,
     MatIcon,
     PersonnelListComponent,
-    MatButton
+    MatButton,
+    MatLabel
   ],
   standalone: true
 })
 export class PersonnelComponent implements OnInit {
   isModalOpen = false;
   selectedMechanic: Mechanic | null = null;
-  mechanics: Mechanic[] = [];
+  filteredMechanics = computed<Mechanic[]>(() => {
+    const filter = this.searchQuery().toLowerCase();
+    return this.mechanics().filter(
+      mechanic =>
+        mechanic.firstName.toLowerCase().includes(filter)
+        || mechanic.lastName.toLowerCase().includes(filter)
+        || mechanic.dni.toString().includes(filter)
+        || mechanic.email.toLowerCase().includes(filter));
+  });
+  mechanics = signal<Mechanic[]>([]);
   noMechanics = false;
-  searchQuery = '';
+  searchQuery = signal('');
 
-  constructor(
-    private personnelService: PersonnelService,
-    private dialog: MatDialog
-  ) {}
+
+
+  constructor( private personnelService: PersonnelService,  private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.getPersonnel();
@@ -46,8 +55,7 @@ export class PersonnelComponent implements OnInit {
   getPersonnel(): void {
     this.personnelService.getByWorkshopId(1).subscribe({
       next: (data: Mechanic[]) => {
-        this.mechanics = data;
-        console.log('Mechanics:', this.mechanics);
+        this.mechanics.set(data);
         this.noMechanics = data.length === 0;
       },
       error: (error) => {
@@ -85,7 +93,7 @@ export class PersonnelComponent implements OnInit {
 
   createMechanic(newMechanicData: Mechanic): void {
     this.personnelService.postPersonnel(newMechanicData).subscribe({
-      next: (data: Mechanic) => this.mechanics.push(data),
+      next: (data: Mechanic) => this.mechanics.set([data, ...this.mechanics()]),
       error: (error) => console.error('Error creating mechanic:', error)
     });
   }
@@ -93,9 +101,9 @@ export class PersonnelComponent implements OnInit {
   updateMechanic(updatedMechanicData: Mechanic): void {
     this.personnelService.putMechanic(updatedMechanicData).subscribe({
       next: () => {
-        const index = this.mechanics.findIndex(m => m.id === updatedMechanicData.id);
+        const index = this.mechanics().findIndex(m => m.id === updatedMechanicData.id);
         if (index !== -1) {
-          this.mechanics[index] = updatedMechanicData;
+          this.mechanics()[index] = updatedMechanicData;
         }
       },
       error: (error) => console.error('Error updating mechanic:', error)
@@ -105,20 +113,9 @@ export class PersonnelComponent implements OnInit {
   deleteMechanic(id: number): void {
     this.personnelService.deleteMechanic(id).subscribe({
       next: () => {
-        this.mechanics = this.mechanics.filter(m => m.id !== id);
+        this.mechanics.set(this.mechanics().filter(m => m.id !== id));
       },
       error: (error) => console.error('Error deleting mechanic:', error)
     });
-  }
-
-  filteredMechanics(): Mechanic[] {
-    if (!this.searchQuery) return this.mechanics;
-    const query = this.searchQuery.toLowerCase();
-    return this.mechanics.filter(mechanic =>
-      mechanic.firstName.toLowerCase().includes(query) ||
-      mechanic.lastName.toLowerCase().includes(query) ||
-      mechanic.dni.includes(query) ||
-      mechanic.email.toLowerCase().includes(query)
-    );
   }
 }
