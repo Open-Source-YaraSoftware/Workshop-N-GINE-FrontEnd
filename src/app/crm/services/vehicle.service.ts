7e238bff1e7ec6d281e514c488f11e33f1bda94a
elmer-riva <elmeraugustoriva@gmail.com>
2024-10-09 09:34:37 -0500

    feat(vehicle-service): add vehicle service with method to get vehicles by client ID

--- src/app/crm/services/vehicle.service.ts
+++ src/app/crm/services/vehicle.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {BaseService} from "../../shared/services/base.service";
 import {Vehicle} from "../../service/model/vehicle.entity";
+import {catchError, retry} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,9 @@ export class VehicleService extends BaseService<Vehicle> {
   constructor() {
     super();
     this.resourceEndpoint = '/vehicles';
+  }
+  getByClientId(clientId: number) {
+    return this.http.get<Vehicle[]>(`${this.resourcePath()}?owner.id=${clientId}`, this.httpOptions)
+      .pipe(retry(2), catchError(this.handleError));
   }
 }
