49d4fa5484a657147032297928b8cbf31f7c087b
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-10 15:36:55 -0500

    refactor(entities): update Checkpoint, Intervention, Mechanic, and Task entities
    - Remove Task property from Checkpoint entity
    - Add description field to Intervention entity
    - Remove embedded list of tasks from Mechanic entity
    - Rename mechanic to assistant in Task entity, keeping the same type

--- src/app/service/model/mechanic.entity.ts
+++ src/app/service/model/mechanic.entity.ts
@@ -1,14 +1,8 @@
-import {Task} from "./task.entity";
 import {User} from "./user.entity";
 
 export class Mechanic extends User {
-  taskList: Task[];
 
   constructor() {
     super();
-    this.taskList = [];
   }
-
-  assignTask(task: Task): void {}
-  completeTask(task: Task): void {}
 }
