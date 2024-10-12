f4a84bd72f78825e499f0ab400fb4d68e8b9d93a
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-12 00:39:56 -0500

    feat(task-product-usage): add TaskProductUsage class to track product usage in tasks
    - Added properties to store taskId, productStockId, quantityUsed, and dateUsed
    - Implemented constructor with object destructuring for initialization

---
+++ src/app/service/model/task-product-usage.entity.spec.ts
@@ -0,0 +1,7 @@
+import { TaskProductUsageEntity } from './task-product-usage.entity';
+
+describe('TaskProductUsageEntity', () => {
+  it('should create an instance', () => {
+    expect(new TaskProductUsageEntity()).toBeTruthy();
+  });
+});
