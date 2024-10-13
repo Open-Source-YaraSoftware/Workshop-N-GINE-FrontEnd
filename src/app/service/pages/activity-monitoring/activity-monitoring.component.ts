7343c2844564bc225005e2a92f6f008d31ab7df4
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-13 02:00:56 -0500

    feat(activity-monitoring): implement ActivityMonitoringComponent to track task status and assigned mechanics
    - Added task table to display task description, assigned mechanic, and task state
    - Integrated personnel data to show mechanic names in the task list
    - Used MatTable with sorting and pagination for a better user experience

--- src/app/service/pages/activity-monitoring/activity-monitoring.component.ts
+++ src/app/service/pages/activity-monitoring/activity-monitoring.component.ts
@@ -1,12 +1,99 @@
-import { Component } from '@angular/core';
+import {Component, inject, signal, ViewChild} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {TaskService} from "../../services/task.service";
+import {Task} from "../../model/task.entity";
+import {
+  MatCell, MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable,
+  MatTableDataSource
+} from "@angular/material/table";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort, MatSortHeader} from "@angular/material/sort";
+import {TaskState} from "../../model/task-state.enum";
+import {PersonnelService} from "../../services/personnel.service";
+import {Mechanic} from "../../model/mechanic.entity";
 
 @Component({
   selector: 'app-activity-monitoring',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTable,
+    MatSort,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatSortHeader,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef,
+    MatPaginator
+  ],
   templateUrl: './activity-monitoring.component.html',
   styleUrl: './activity-monitoring.component.css'
 })
 export class ActivityMonitoringComponent {
+  private taskService: TaskService = inject(TaskService);
+  private personnelService: PersonnelService = inject(PersonnelService);
+  private route = inject(ActivatedRoute);
+  private personnel = signal<Mechanic[]>([]);
+  protected interventionId= signal(0);
+  protected dataSource = new MatTableDataSource<Task>([]);
+  protected displayedColumns: string[] = ['description', 'mechanic','state'];
+  protected readonly TaskState = TaskState;
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  ngOnInit() {
+    this.getPersonnel();
+    this.searchQueryParams();
+  }
+
+  public load(){
+    this.getTasks();
+  }
+
+  private searchQueryParams() {
+    this.route.params
+      .subscribe(params => {
+        this.interventionId.set(params['id'] || 0);
+        this.getTasks();
+      });
+  }
+
+  private getTasks() {
+    this.taskService.getByInterventionId(this.interventionId())
+      .subscribe(tasks => {
+        this.dataSource.data = tasks;
+        this.getTaskWithMechanicName();
+      });
+  }
+
+  private getPersonnel() {
+    this.personnelService.getAll()
+      .subscribe(personnel => {
+        this.personnel.set(personnel);
+      });
+  }
+
+  private getTaskWithMechanicName() {
+    this.dataSource.data = this.dataSource.data.map(task => {
+      const mechanic = this.personnel().find(p => p.id === task.assistant.id);
+      return {
+        ...task,
+        mechanic: mechanic ? `${mechanic.firstName} ${mechanic.lastName}` : 'Unknown'
+      };
+    });
+  }
 }
