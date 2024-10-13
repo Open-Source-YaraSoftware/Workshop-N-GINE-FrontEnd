dd7b9288def8d0a1facebf3ba8aaab630bed14b5
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-13 04:28:31 -0500

    feat(activity): add finish intervention logic and confirmation dialog
    - Added `onFinishIntervention` method to handle intervention completion with confirmation dialog.
    - Integrated intervention state update through `InterventionsService` with feedback notification.

--- src/app/service/pages/activity/activity.component.ts
+++ src/app/service/pages/activity/activity.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, inject, signal, ViewChild} from '@angular/core';
 import {
   MatStep,
   MatStepLabel,
@@ -17,6 +17,14 @@ import {ActivityExecutionComponent} from "../../components/activity-execution/ac
 import {ActivityMonitoringComponent} from "../../components/activity-monitoring/activity-monitoring.component";
 import {ActivatedRoute} from "@angular/router";
 import {NgIf} from "@angular/common";
+import {MatDialog} from "@angular/material/dialog";
+import {NotificationMessagesService} from "../../../shared/services/notification-messages.service";
+import {
+  ConfirmationDialogComponent
+} from "../../../shared/components/confirmation-dialog/confirmation-dialog.component";
+import {InterventionsService} from "../../services/interventions.service";
+import {Intervention} from "../../model/intervention.entity";
+import {InterventionState} from "../../model/intervention-state.enum";
 
 @Component({
   selector: 'app-activity',
@@ -43,13 +51,23 @@ export class ActivityComponent {
   type: string = 'leader';
   @ViewChild('execution') execution!: ActivityExecutionComponent;
   @ViewChild('supervision') supervision!: ActivityMonitoringComponent;
+  private confirmDialogRef = inject(MatDialog);
+  private notificationMessagesService:NotificationMessagesService = inject(NotificationMessagesService);
+  private interventionService: InterventionsService = inject(InterventionsService);
+  protected intervention = signal<Intervention>(new Intervention());
+  protected interventionId= signal(0);
 
   constructor(private route: ActivatedRoute) {
     this.searchQueryParams();
+    this.searchParams();
+    this.getIntervention();
   }
 
-  ngOnInit() {
-    this.searchQueryParams();
+  private getIntervention() {
+    this.interventionService.getById(this.interventionId())
+      .subscribe((intervention: Intervention) => {
+        this.intervention.set(intervention);
+      });
   }
 
   protected onStepChange(event: any) {
@@ -77,10 +95,36 @@ export class ActivityComponent {
     }
   }
 
+  private searchParams() {
+    this.route.params
+      .subscribe(params => {
+        this.interventionId.set(params['id'] || 0);
+      });
+  }
+
   private searchQueryParams() {
     this.route.queryParams
       .subscribe(params => {
         this.type = params['type'] || 'leader';
+      });
+  }
+
+  protected onFinishIntervention() {
+    const dialog = this.confirmDialogRef.open(ConfirmationDialogComponent, {  data: { message: 'Are you sure you want to update this?' } });
+    dialog.componentInstance.confirm.subscribe(()=>this.finishIntervention());
+  }
+
+  private finishIntervention() {
+    const intervention = this.intervention();
+    intervention.state = InterventionState.COMPLETED;
+    this.interventionService.update(intervention.id, intervention)
+      .subscribe({
+        next: () => {
+          this.notificationMessagesService.openMessage('Intervention completed successfully',1 );
+        },
+        error: () => {
+          this.notificationMessagesService.openMessage('Error completing intervention ', 3);
+        }
       });
   }
 }
