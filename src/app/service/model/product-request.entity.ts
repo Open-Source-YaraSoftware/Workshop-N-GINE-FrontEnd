ca4d0341540ff0ed4772eb25cd86105940748cc9
Eric <erich56000@gmail.com>
2024-10-12 19:06:35 -0500

    feat(product-request): update product-request entity

--- src/app/service/model/product-request.entity.ts
+++ src/app/service/model/product-request.entity.ts
@@ -1,23 +1,32 @@
 import {Workshop} from "./workshop.entity";
 import {Task} from "./task.entity";
 import {ProductRequestState} from "./product-request-state.enum";
+import {ProductType} from "./product-type.entity";
 
 export class ProductRequest {
   id: number;
   workshop: Workshop;
   task: Task;
-  requestedDate: Date;
+  requestedDate: String;
   requestedQuantity: number;
   status: ProductRequestState;
   name: string;
 
-  constructor() {
-    this.id = 0;
-    this.workshop = new Workshop();
-    this.task = new Task();
-    this.requestedDate = new Date();
-    this.requestedQuantity = 0;
-    this.status = ProductRequestState.PENDING;
-    this.name = '';
+  constructor({
+                id = 0,
+                workshop = new Workshop(),
+                task = new Task(),
+                requestedDate = '',
+                requestedQuantity = 0,
+                status = ProductRequestState.PENDING,
+                name =  ''
+              }={}) {
+    this.id = id;
+    this.workshop = workshop;
+    this.task = task;
+    this.requestedDate = requestedDate;
+    this.requestedQuantity = requestedQuantity;
+    this.status = status;
+    this.name = name;
   }
 }
