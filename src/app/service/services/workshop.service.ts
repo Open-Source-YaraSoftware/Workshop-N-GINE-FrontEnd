032dcff2c74e1639d6ad0ee171ea266522f26945
elmer-riva <elmeraugustoriva@gmail.com>
2024-11-19 13:29:34 -0500

    feat: add workshop service and entity

---
+++ src/app/service/services/workshop.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import {BaseService} from "../../shared/services/base.service";
+import {Workshop} from "../model/workshop.entity";
+import {catchError, Observable, retry} from "rxjs";
+import {Intervention} from "../model/intervention.entity";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WorkshopService extends BaseService<Workshop>{
+
+  constructor() {
+    super();
+    this.resourceEndpoint = '/workshops';
+  }
+
+  public getAllInterventionsByWorkshopId(workshopId: number) :Observable<Intervention[]> {
+    return this.http.get<Intervention[]>(`${this.resourcePath()}/${workshopId}/interventions`, this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      );
+  }
+
+  public getClientsIdsByWorkshopId(workshopId: number) :Observable<number[]> {
+    return this.http.get<number[]>(`${this.resourcePath()}/${workshopId}/clients`, this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      );
+  }
+
+  public getMechanicsIdsByWorkshopId(workshopId: number) :Observable<number[]> {
+    return this.http.get<number[]>(`${this.resourcePath()}/${workshopId}/mechanics`, this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      );
+  }
+}
