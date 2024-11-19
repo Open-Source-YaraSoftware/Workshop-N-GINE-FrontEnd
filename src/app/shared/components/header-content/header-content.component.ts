8242fb277036bd6216b3c42cf4aee9d72c1a74cf
Johan Quiñones <91556525+johaanq@users.noreply.github.com>
2024-11-19 03:27:26 -0500

    feat: add language-switcher buttons in header-content component

--- src/app/shared/components/header-content/header-content.component.ts
+++ src/app/shared/components/header-content/header-content.component.ts
@@ -3,6 +3,7 @@ import { MatToolbar } from "@angular/material/toolbar";
 import { MatIcon } from "@angular/material/icon";
 import { MatIconButton, MatButtonModule } from "@angular/material/button";
 import { RouterModule } from '@angular/router';
+import {LanguageSwitcherComponent} from "../language-switcher/language-switcher.component";
 
 @Component({
   selector: 'app-header-content',
@@ -12,7 +13,8 @@ import { RouterModule } from '@angular/router';
     MatIcon,
     MatIconButton,
     MatButtonModule,
-    RouterModule
+    RouterModule,
+    LanguageSwitcherComponent
   ],
   templateUrl: './header-content.component.html',
   styleUrls: ['./header-content.component.css']
