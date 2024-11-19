2bb5cbeab42b3ff34a13cb12a67c1b86cd0337d7
Eric <erich56000@gmail.com>
2024-11-19 11:48:29 -0500

    refactor(activity-error) refactor activity request to momentarily work fine even with the changes made

--- src/app/service/components/activity-requests/activity-requests.component.ts
+++ src/app/service/components/activity-requests/activity-requests.component.ts
@@ -138,9 +138,9 @@ export class ActivityRequestsComponent {
     productRequest.taskId = this.taskSelectedId;
     const otherPartValue = this.requestsForm.get('otherPart')?.value;
     if(!otherPartValue) return;
-    productRequest.name = otherPartValue;
+    //productRequest.name = otherPartValue;
     const observationValue = this.requestsForm.get('observation')?.value;
-    productRequest.observation = observationValue? observationValue: '';
+    //productRequest.observation = observationValue? observationValue: '';
     productRequest.requestedQuantity = Number(this.requestsForm.get('quantity')?.value);
     this.productRequestService.create(productRequest)
       .subscribe({
@@ -231,8 +231,8 @@ export class ActivityRequestsComponent {
   private editProductRequest(productRequest: ProductRequest) {
     this.productRequestSelected.set(productRequest);
     this.requestsForm.get('requiredPart')?.setValue(0);
-    this.requestsForm.get('otherPart')?.setValue(productRequest.name);
-    this.requestsForm.get('observation')?.setValue(productRequest.observation);
+    // this.requestsForm.get('otherPart')?.setValue(productRequest.name);
+    // this.requestsForm.get('observation')?.setValue(productRequest.observation);
     this.requestsForm.get('quantity')?.setValue(productRequest.requestedQuantity);
   }
 
@@ -271,11 +271,11 @@ export class ActivityRequestsComponent {
   private updateProductRequest() {
     const productRequest = this.productRequestSelected();
     const otherPartValue = this.requestsForm.get('otherPart')?.value;
-    if(otherPartValue) productRequest.name = otherPartValue;
+    //if(otherPartValue) productRequest.name = otherPartValue;
     const observationValue = this.requestsForm.get('observation')?.value;
-    if(observationValue) productRequest.observation = observationValue;
-    productRequest.requestedQuantity = Number(this.requestsForm.get('quantity')?.value);
-    productRequest.requestedDate = new Date();
+    //if(observationValue) productRequest.observation = observationValue;
+   // productRequest.requestedQuantity = Number(this.requestsForm.get('quantity')?.value);
+    //productRequest.requestedDate = new Date();
     this.productRequestService.update(productRequest.id, productRequest)
       .subscribe({
         next: ()=>{
